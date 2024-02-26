ea12febf8588ad09e2cd60378e13b4ba1f604704
piragenth-ltfb <linuxtutorialforbeginner@gmail.com>
2024-02-26 15:26:16 +0530

    prime

--- prime.java
+++ prime.java
@@ -1,19 +1,20 @@
-public class prime {
-    public static void main (String [] args){
-    int value_not_prime = 0;
-    for (int i = 2; i<100; i++){
-        for (int a = 1; a<i; a++){
-           if (i%a == 0){
-                value_not_prime = 1;               
+public class prime{
+    public static void main (String args[]){
+        int value_not_prime=0;
+        for (int i = 1;i < 1000000000; i++){
+            for (int j = 2; j<i; j++){
+                if (i/2 <= j){
+                    break;
+                }
+                if (i%j == 0){
+                    value_not_prime =1;
+                    break;
+                }
             }
-        }
-        if (value_not_prime == 1){ 
-            continue;
-        }
-        else{
-            System.out.println(i);
+            if (value_not_prime==0){
+                System.out.println(i);
             }
-        value_not_prime = 0;
-        }
-    
-}}
+            value_not_prime = 0;
+        } 
+    }
+}
